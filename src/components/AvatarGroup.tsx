b7bffd0b26dcaf9d5e0332770402b0ae12c2cfcf
Dev Prasad Sethi <147725410+Devsethi3@users.noreply.github.com>
2025-05-31 23:14:41 +0530

    Update AvatarGroup.tsx

--- src/components/AvatarGroup.tsx
+++ src/components/AvatarGroup.tsx
@@ -4,7 +4,7 @@ import { motion } from 'framer-motion';
 import { useRef } from 'react';
 import { useInView } from 'framer-motion';
 
-// Sample ava
+// Sample av
 const avatars = [
   {
     id: 1,
