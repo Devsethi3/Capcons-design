1e449b57c26a136c05950489ccd76ac193366023
Dev Prasad Sethi <147725410+Devsethi3@users.noreply.github.com>
2025-06-05 22:49:08 +0530

    Update BlogSection.tsx

--- src/components/BlogSection.tsx
+++ src/components/BlogSection.tsx
@@ -36,7 +36,7 @@ const BlogSection = () => {
         </button>
       </div>
 
-      {/* Blog Car */}
+      {/* Blog Ca */}
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
         {blogPosts.map((post) => (
           <div
