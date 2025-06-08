65d0e71254f483d6960054340af1ce9ee2710780
Dev Prasad Sethi <147725410+Devsethi3@users.noreply.github.com>
2025-06-08 22:42:13 +0530

    Update BlogSection.tsx

--- src/components/BlogSection.tsx
+++ src/components/BlogSection.tsx
@@ -42,7 +42,6 @@ const BlogSection = () => {
             key={post.id}
             className="bg-white rounded-2xl overflow-hidden shadow-lg hover:shadow-xl transition-shadow duration-300"
           >
-            {/* */}
             <div className="aspect-video w-full overflow-hidden">
               <img
                 src={post.image}
