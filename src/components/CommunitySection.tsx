bc6c43172b2640547cfd14041921026f6a3a88fe
Dev Prasad Sethi <147725410+Devsethi3@users.noreply.github.com>
2025-06-28 23:26:18 +0530

    Update CommunitySection.tsx

--- src/components/CommunitySection.tsx
+++ src/components/CommunitySection.tsx
@@ -76,7 +76,7 @@ const CommunitySection = () => {
           />
         </motion.div>
 
-        {/* C */}
+        {/* */}
         <div className="relative z-20 text-center max-w-4xl mx-auto py-20">
           <motion.h2
             initial={{ opacity: 0, y: 30 }}
