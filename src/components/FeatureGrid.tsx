cc1cc928d09325f111867c5c9b1d8e6695de6d94
Dev Prasad Sethi <147725410+Devsethi3@users.noreply.github.com>
2025-06-29 22:44:49 +0530

    Update FeatureGrid.tsx

--- src/components/FeatureGrid.tsx
+++ src/components/FeatureGrid.tsx
@@ -33,7 +33,7 @@ const CreatorConnect = () => {
         animate="visible"
         variants={containerVariants}
       >
-        {/* He */}
+        {/* e */}
         <motion.div
           className="text-center mb-12 lg:mb-16"
           variants={itemVariants}
