465fa206dd750fa6430d394dbb9ae58eac045f59
Dev Prasad Sethi <147725410+Devsethi3@users.noreply.github.com>
2025-07-15 23:12:17 +0530

    Update Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -14,7 +14,7 @@ const Footer = () => {
             Join now and build your communications!
           </p>
 
-          {/* App download butt */}
+          {/* App download but */}
           <div className="flex flex-col sm:flex-row justify-center items-center gap-4 mb-12">
             <Image
               src="/googleplay.png"
