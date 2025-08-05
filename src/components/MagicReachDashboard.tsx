a0b8d24aa48842fa1c94b98e4ea839f335e7dda6
Dev Prasad Sethi <147725410+Devsethi3@users.noreply.github.com>
2025-08-05 23:28:06 +0530

    Update MagicReachDashboard.tsx

--- src/components/MagicReachDashboard.tsx
+++ src/components/MagicReachDashboard.tsx
@@ -48,7 +48,7 @@ const MagicReachDashboard: React.FC = () => {
         animate="visible"
       >
         <div className="w-full flex items-center gap-6 md:gap-10 pb-4">
-          {/* Left Section - Magic Rea */}
+          {/* Left Section - Magic Re */}
 
           {/* Center Section - 7-Day XP Challenge */}
           <motion.div
