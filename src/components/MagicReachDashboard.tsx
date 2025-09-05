e56086c75c8dcb33b13456277464ca5a2d8862d8
Dev Prasad Sethi <147725410+Devsethi3@users.noreply.github.com>
2025-09-05 23:19:25 +0530

    Update MagicReachDashboard.tsx

--- src/components/MagicReachDashboard.tsx
+++ src/components/MagicReachDashboard.tsx
@@ -160,7 +160,6 @@ const MagicReachDashboard: React.FC = () => {
             </div>
           </motion.div>
 
-          {/* */}
           <motion.div
             className="h-[70vh] min-w-[280px] md:min-w-[320px] flex flex-col gap-6 flex-shrink-0"
             variants={itemVariants}
