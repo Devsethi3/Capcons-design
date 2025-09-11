aa638f19ce7229290c6721408ee47582ba18a799
Dev Prasad Sethi <147725410+Devsethi3@users.noreply.github.com>
2025-09-11 23:28:00 +0530

    Update MagicReachDashboard.tsx

--- src/components/MagicReachDashboard.tsx
+++ src/components/MagicReachDashboard.tsx
@@ -179,7 +179,7 @@ const MagicReachDashboard: React.FC = () => {
                 industry.
               </p>
 
-              {/* Deco */}
+              {/* Dec */}
               <div className="absolute bottom-2 right-2 opacity-20">
                 <motion.div
                   animate={{ rotate: 360 }}
