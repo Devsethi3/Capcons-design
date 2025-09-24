6cf97626927eb732f5ec1a6352279393ae528c94
Dev Prasad Sethi <147725410+Devsethi3@users.noreply.github.com>
2025-09-24 23:24:26 +0530

    Update Navbar.tsx

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -17,7 +17,7 @@ const Navbar = () => {
     const handleScroll = () => {
       const currentScrollY = window.scrollY;
 
-      // Show
+      // Sho
       if (currentScrollY > lastScrollY && currentScrollY > 100) {
         setShowNavbar(false);
       } else {
