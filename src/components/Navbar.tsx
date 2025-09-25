5fc1d6f6f23f41221895c5b2fa2d9f867330b3a8
Dev Prasad Sethi <147725410+Devsethi3@users.noreply.github.com>
2025-09-25 23:07:36 +0530

    Update Navbar.tsx

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -23,7 +23,7 @@ const Navbar = () => {
         setShowNavbar(true);
       }
 
-      // Add background blur when scrolle
+      // Add background blur when scroll
       setIsScrolled(currentScrollY > 50);
       setLastScrollY(currentScrollY);
     };
