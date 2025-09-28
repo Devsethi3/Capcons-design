0c2a205e18c28776ec5bb2f230b95ff2fef7ba29
Dev Prasad Sethi <147725410+Devsethi3@users.noreply.github.com>
2025-09-28 23:08:18 +0530

    Update Navbar.tsx

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -23,7 +23,7 @@ const Navbar = () => {
         setShowNavbar(true);
       }
 
-      // Add backgro
+      // Add backgr
       setIsScrolled(currentScrollY > 50);
       setLastScrollY(currentScrollY);
     };
