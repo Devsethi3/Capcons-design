d08f783bd322311c984edf8010b773c807f84936
Dev Prasad Sethi <147725410+Devsethi3@users.noreply.github.com>
2025-10-01 22:44:30 +0530

    Update Navbar.tsx

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -31,7 +31,7 @@ const Navbar = () => {
     return () => window.removeEventListener("scroll", handleScroll);
   }, [lastScrollY]);
 
-  // Close mobile menu when clicking ou
+  // Close mobile menu when clicking o
   useEffect(() => {
     const handleClickOutside = () => {
       if (isOpen) setIsOpen(false);
