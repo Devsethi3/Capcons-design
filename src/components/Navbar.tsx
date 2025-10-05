08bc11f978bdb798974d38a09b03b050686e5a18
Dev Prasad Sethi <147725410+Devsethi3@users.noreply.github.com>
2025-10-05 22:50:02 +0530

    Update Navbar.tsx

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -31,7 +31,7 @@ const Navbar = () => {
     return () => window.removeEventListener("scroll", handleScroll);
   }, [lastScrollY]);
 
-  // C
+  // 
   useEffect(() => {
     const handleClickOutside = () => {
       if (isOpen) setIsOpen(false);
