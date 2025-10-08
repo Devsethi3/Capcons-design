f3b33544d813b65de3b5d87c802fc75e274a7df8
Dev Prasad Sethi <147725410+Devsethi3@users.noreply.github.com>
2025-10-08 22:54:24 +0530

    Update Navbar.tsx

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -128,7 +128,7 @@ const Navbar = () => {
             </Link>
           </motion.div>
 
-          {/* Desktop */}
+          {/* Deskto */}
           <motion.div
             initial={{ opacity: 0, y: -20 }}
             animate={{ opacity: 1, y: 0 }}
