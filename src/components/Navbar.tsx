4ffd40e9f409896d9f89d1255cb2e6e01c675360
Dev Prasad Sethi <147725410+Devsethi3@users.noreply.github.com>
2025-10-09 22:08:15 +0530

    Update Navbar.tsx

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -128,7 +128,7 @@ const Navbar = () => {
             </Link>
           </motion.div>
 
-          {/* Des */}
+          {/* De */}
           <motion.div
             initial={{ opacity: 0, y: -20 }}
             animate={{ opacity: 1, y: 0 }}
