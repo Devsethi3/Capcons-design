f08a4d21cf73d87a5c8c5357e7b24f8b09733948
Dev Prasad Sethi <147725410+Devsethi3@users.noreply.github.com>
2025-10-11 23:01:02 +0530

    Update Navbar.tsx

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -164,7 +164,7 @@ const Navbar = () => {
             </Link>
           </motion.div>
 
-          {/* Desktop Auth */}
+          {/* Desktop Aut */}
           <motion.div
             initial={{ opacity: 0, x: 20 }}
             animate={{ opacity: 1, x: 0 }}
