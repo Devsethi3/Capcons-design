9c357e00b1112bd089cbee3e4a7ce852fc78216f
Dev Prasad Sethi <147725410+Devsethi3@users.noreply.github.com>
2025-10-15 22:28:03 +0530

    Update Navbar.tsx

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -184,7 +184,7 @@ const Navbar = () => {
             </Link>
           </motion.div>
 
-          {/* Mobil */}
+          {/* Mobi */}
           <div className="md:hidden w-full flex items-center justify-between">
             <motion.button
               whileTap={{ scale: 0.95 }}
