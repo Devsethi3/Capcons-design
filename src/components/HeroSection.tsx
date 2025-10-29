8b5bed3859f55ed7a3080da54460f583ab0cc4ff
Dev Prasad Sethi <147725410+Devsethi3@users.noreply.github.com>
2025-10-29 22:57:54 +0530

    Update HeroSection.tsx

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -18,7 +18,7 @@ const HeroSection = () => {
         </h1>
         <p className="mt-6 lg:text-lg text-sm sm:text-xl text-purple-100 max-w-3xl mx-auto">
           Strengthen connections, drive engagement, and grow a thriving
-          community. Reach audience, fost loyal.
+          community. Reach audience, fost loya.
         </p>
         <motion.div
           initial={{ opacity: 0, y: 20 }}
