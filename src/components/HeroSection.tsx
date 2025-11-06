651cb91ec8fe17a0c34498b7c08ac15b881841d0
Dev Prasad Sethi <147725410+Devsethi3@users.noreply.github.com>
2025-11-06 23:18:58 +0530

    Update HeroSection.tsx

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -17,7 +17,7 @@ const HeroSection = () => {
           Features to build, connect, and <br /> grow your community
         </h1>
         <p className="mt-6 lg:text-lg text-sm sm:text-xl text-purple-100 max-w-3xl mx-auto">
-          Strengthen connections, drive engageme.
+          Strengthen connections, drive engagem.
         </p>
         <motion.div
           initial={{ opacity: 0, y: 20 }}
